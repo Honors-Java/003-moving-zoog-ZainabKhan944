698b0c7e8b662e2955af769738824d9111b9f1fc
ZainabKhan944 <zyk0517@gmail.com>
2020-09-25 14:59:01 +0000

    I added the eyes and the legs

--- index.java
+++ index.java
@@ -15,7 +15,7 @@ void draw() {
   // Draw Zoog's body
   stroke(0);
   fill(150);
-  rect(mouseX, mouseY,20,100);
+  rect(mouseX, mouseY,20,100); 
   // Draw Zoog's head
   stroke(0);
   fill(255);
@@ -23,12 +23,16 @@ void draw() {
   // Draw Zoog's eyes
   fill(0);
 
+
+
   // remove the forward slashes in front of each line of code below one at a time. Remove the underline and replace with the proper code to allow your mouse to move Zoog around  💻
 
-  //ellipse(__________, __________,16,32);
-  //ellipse(__________, __________,16,32);
+  ellipse(mouseX -18, mouseY -30,16,32);
+  ellipse(mouseX +18, mouseY -30,16,32); 
+
+
   // Draw Zoog's legs
   stroke(0);
-  //line(_________, __________, __________, __________);
-  //line(__________, __________, __________, __________);
+  line(mouseX +10, mouseY +50, mouseX +20, mouseY +60);
+  line(mouseX -10, mouseY +50, mouseX -20, mouseY +60);
 }
